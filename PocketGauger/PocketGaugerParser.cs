1fbe155ae0f97f1bb85f22a91d7c363e20fbed30
Cormac Brennan <cormac.brennan@aquaticinformatics.com>
2016-05-16 15:48:10 -0700

    AQ-18918 Address review comments
    Removed comment, empty PropertyGroup and unused references from Server.Plugins.FieldVisit.PocketGauger.csproj
    Change default project to x64
    PocketGaugerParser implements IFieldDataPlugin

--- PocketGauger/PocketGaugerParser.cs
+++ PocketGauger/PocketGaugerParser.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using log4net;
+using Server.BusinessInterfaces.FieldDataPlugInCore;
 using Server.BusinessInterfaces.FieldDataPlugInCore.Context;
 using Server.BusinessInterfaces.FieldDataPlugInCore.Results;
 
 namespace Server.Plugins.FieldVisit.PocketGauger
 {
-    public class PocketGaugerParser
+    public class PocketGaugerParser : IFieldDataPlugIn
     {
         public ICollection<ParsedResult> ParseFile(Stream fileStream, IParseContext context, ILog logger)
         {
