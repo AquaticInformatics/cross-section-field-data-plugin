d791c194593adc8b50b78487df24af29bad19b31
Cormac Brennan <cormac.brennan@aquaticinformatics.com>
2016-05-16 14:12:24 -0700

    AQ-18918 Create PocketGauger project

---
+++ PocketGauger/PocketGaugerParser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using log4net;
+using Server.BusinessInterfaces.FieldDataPlugInCore.Context;
+using Server.BusinessInterfaces.FieldDataPlugInCore.Results;
+
+namespace Server.Plugins.FieldVisit.PocketGauger
+{
+    public class PocketGaugerParser
+    {
+        public ICollection<ParsedResult> ParseFile(Stream fileStream, IParseContext context, ILog logger)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
