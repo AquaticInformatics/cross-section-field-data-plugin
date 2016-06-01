9d363a8598a2dc9dc7958a2535b3a119a828d6cc
Martin Benn <martin.benn@aquaticinformatics.com>
2016-05-31 21:55:30 -0700

    AQ-18922 Get Location Default Channel
    - Attempt to get the 'Main' channel from the location. If the location has no channels, then one is created. If the location has channels, but none of them have the name 'Main' then the first one in the list is returned.

---
+++ PocketGauger.UnitTests/Helpers/ChannelHelperTests.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NSubstitute;
+using NUnit.Framework;
+using Server.BusinessInterfaces.FieldDataPlugInCore.Context;
+using Server.Plugins.FieldVisit.PocketGauger.Helpers;
+
+namespace Server.Plugins.FieldVisit.PocketGauger.UnitTests.Helpers
+{
+    [TestFixture]
+    public class ChannelHelperTests
+    {
+        private ILocationInfo _locationInfo;
+
+        [SetUp]
+        public void SetupForEachTest()
+        {
+            _locationInfo = Substitute.For<ILocationInfo>();
+
+            _locationInfo.Channels.ReturnsForAnyArgs(new List<IChannelInfo>());
+
+            var mainChannel = CreateMainChannel();
+            _locationInfo.CreateNewChannel(Arg.Any<string>()).Returns(mainChannel);
+        }
+
+        private static IChannelInfo CreateMainChannel()
+        {
+            return CreateChannel(ChannelHelper.MainChannel);
+        }
+
+        private static IChannelInfo CreateChannel(string channelName)
+        {
+            var channel = Substitute.For<IChannelInfo>();
+            channel.ChannelName.ReturnsForAnyArgs(channelName);
+
+            return channel;
+        }
+
+        [Test]
+        public void GetDefaultLocationChannel_LocationHasNoChannels_CallsCreateNewChannelOnLocation()
+        {
+            ChannelHelper.GetDefaultLocationChannel(_locationInfo);
+
+            _locationInfo.Received().CreateNewChannel(ChannelHelper.MainChannel);
+        }
+
+        [Test]
+        public void GetDefaultLocationChannel_LocationHasMainChannel_ReturnsMainChannel()
+        {
+            var mainChannel = CreateMainChannel();
+
+            _locationInfo.Channels.ReturnsForAnyArgs(new List<IChannelInfo> { mainChannel });
+
+            var channel =  ChannelHelper.GetDefaultLocationChannel(_locationInfo);
+
+            Assert.That(channel, Is.EqualTo(mainChannel));
+        }
+
+        [Test]
+        public void GetDefaultLocationChannel_LocationNoHasMainChannel_ReturnsFirstChannelInList()
+        {
+            var channels = new List<IChannelInfo>
+            {
+                CreateChannel("Abc"),
+                CreateChannel("Other"),
+                CreateChannel("Main (Left)")
+            };
+
+            _locationInfo.Channels.ReturnsForAnyArgs(channels);
+
+            var channel = ChannelHelper.GetDefaultLocationChannel(_locationInfo);
+
+            Assert.That(channel, Is.EqualTo(channels.First()));
+        }
+    }
+}
