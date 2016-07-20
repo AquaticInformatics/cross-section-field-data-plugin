e73be933a144f5840cc53cb5798cbbb639422848
Martin Benn <martin.benn@aquaticinformatics.com>
2016-07-20 16:52:54 -0700

    AQ-19245 Create empty cross section plugin projects
    - Create empty CrossSection plugin and test projects in the Server.FieldVisit solution.

---
+++ CrossSection/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Server.Plugins.FieldVisit.CrossSection")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
