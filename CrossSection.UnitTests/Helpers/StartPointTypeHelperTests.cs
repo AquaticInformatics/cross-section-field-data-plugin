a19cf4b8c2ba30bfdb3badbeb289ccb93857ff65
Martin Benn <martin.benn@aquaticinformatics.com>
2016-07-27 13:40:05 -0700

    AQ-19245 Type parser helpers for Double, DateTimeOffset and StartPointType
    - Added DateTimeOffsetHelper for parsing an ISO 8601 formatted string into DateTimeOffset
    - StartPointTypeHelper attempts to parse StartPointType enum from the string value.

---
+++ CrossSection.UnitTests/Helpers/StartPointTypeHelperTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Server.BusinessInterfaces.FieldDataPlugInCore.DataModel.DischargeSubActivities;
+using Server.Plugins.FieldVisit.CrossSection.Helpers;
+
+namespace Server.Plugins.FieldVisit.CrossSection.UnitTests.Helpers
+{
+    [TestFixture]
+    public class StartPointTypeHelperTests
+    {
+        [TestCaseSource(nameof(InvalidStartPointTypeStrings))]
+        public void Parse_ValueIsNotValid_ReturnsDefaultStartPointType(string invalidOrUnknownStartPointTypeStrings)
+        {
+            var startPoint = StartPointTypeHelper.Parse(invalidOrUnknownStartPointTypeStrings);
+
+            Assert.That(startPoint, Is.EqualTo(CrossSectionParserConstants.DefaultStartPointType));
+        }
+
+        private static readonly List<string> InvalidStartPointTypeStrings = new List<string>
+        {
+            "L",
+            "R",
+            Guid.NewGuid().ToString(),
+            "",
+            null,
+            "About the LeftBank"
+        };
+
+        [TestCaseSource(nameof(LeftEdgeOfWaterTestCases))]
+        public void Parse_ValueIsExpectedLeftEdgeOfWater_ReturnsLeftEdgeOfWater(string leftEdgeOfWaterString)
+        {
+            var result = StartPointTypeHelper.Parse(leftEdgeOfWaterString);
+
+            Assert.That(result, Is.EqualTo(StartPointType.LeftEdgeOfWater));
+        }
+
+        private static readonly List<string> LeftEdgeOfWaterTestCases = new List<string>
+        {
+            "LeftEdgeOfWater",
+            "Left Edge Of Water",
+            "Left edge OF WATER",
+            "LEfTEdGEOFWaTER",
+            "Left",
+            "LeftBank"
+        };
+
+        [TestCaseSource(nameof(RightEdgeOfWaterTestCases))]
+        public void Parse_ValueIsExpectedRightEdgeOfWater_ReturnsRightEdgeOfWater(string rightEdgeOfWaterString)
+        {
+            var result = StartPointTypeHelper.Parse(rightEdgeOfWaterString);
+
+            Assert.That(result, Is.EqualTo(StartPointType.RightEdgeOfWater));
+        }
+
+        private static readonly List<string> RightEdgeOfWaterTestCases = new List<string>
+        {
+            "RightEdgeOfWater",
+            "Right Edge Of Water",
+            "RIGHT edge OF WATER",
+            "RightEdGEOFWaTER",
+            "Right",
+            "RightBank"
+        };
+    }
+}
---
+++ CrossSection/Helpers/StartPointTypeHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Server.BusinessInterfaces.FieldDataPlugInCore.DataModel.DischargeSubActivities;
+
+namespace Server.Plugins.FieldVisit.CrossSection.Helpers
+{
+    public class StartPointTypeHelper
+    {
+        private const StartPointType DefaultStartPointType = CrossSectionParserConstants.DefaultStartPointType;
+        private const string LeftPrefix = "Left";
+        private const string RightPrefix = "Right";
+
+        public static StartPointType Parse(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return DefaultStartPointType;
+
+            var startPoint = value.Replace(" ", string.Empty);
+
+            StartPointType startPointEnum;
+
+            if (Enum.TryParse(startPoint, true, out startPointEnum))
+                return startPointEnum;
+
+            return AttemptToInferStartPoint(startPoint);
+        }
+
+        private static StartPointType AttemptToInferStartPoint(string startPoint)
+        {
+            if (startPoint.StartsWith(LeftPrefix, StringComparison.OrdinalIgnoreCase))
+                return StartPointType.LeftEdgeOfWater;
+
+            if (startPoint.StartsWith(RightPrefix, StringComparison.OrdinalIgnoreCase))
+                return StartPointType.RightEdgeOfWater;
+
+            return DefaultStartPointType;
+        }
+    }
+}
