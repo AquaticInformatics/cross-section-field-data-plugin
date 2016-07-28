73e280096bdea00484cadbd2497c22b60438ca00
Martin Benn <martin.benn@aquaticinformatics.com>
2016-07-28 16:28:44 -0700

    AQ-19245 Remove unused usings in Field Visit projects

--- CrossSection.UnitTests/TestData/TestHelpers.cs
+++ CrossSection.UnitTests/TestData/TestHelpers.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using NSubstitute;
 using Ploeh.AutoFixture;
 using Server.BusinessInterfaces.FieldDataPlugInCore.Context;
