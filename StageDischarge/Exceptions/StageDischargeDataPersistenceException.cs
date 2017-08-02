ed92400b5e431428de96f3f6bb0cd8ea8c035abf
Tom Smith <tom.smith@aquaticinformatics.com>
2017-08-02 10:52:25 -0700

    AQ-21444 Project folders in place for new plugin and tests

---
+++ StageDischarge/Exceptions/StageDischargeDataPersistenceException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Server.Plugins.FieldVisit.StageDischarge.Exceptions
+{
+    [Serializable]
+    public class StageDischargeDataPersistenceException : Exception
+    {
+        public StageDischargeDataPersistenceException()
+        {
+        }
+
+        public StageDischargeDataPersistenceException(string message) : base(message)
+        {
+        }
+
+        public StageDischargeDataPersistenceException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected StageDischargeDataPersistenceException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
