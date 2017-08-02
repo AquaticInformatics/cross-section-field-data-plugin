1fc50db5c3c0eb6776bdd1fc4471ef5282ca0da2
Tom Smith <tom.smith@aquaticinformatics.com>
2017-08-02 10:52:25 -0700

    AQ-21444 Project folders in place for new plugin and tests

---
+++ StageDischarge.UnitTests/StageDischargeParserTests.cs
@@ -0,0 +1,11 @@
+using Common.TestHelpers.NUnitExtensions;
+using NUnit.Framework;
+
+namespace Server.Plugins.FieldVisit.StageDischarge.UnitTests
+{
+    [TestFixture]
+    [LongRunning]
+    public class StageDischargeParserTests
+    {
+    }
+}
